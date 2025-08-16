f99694bf7414a0b2a34c6626330a6f6182e91152
JNACHO2001 <113179500+JNACHO2001@users.noreply.github.com>
2025-08-16 17:32:07 -0500

    feat:conexion ala base de datos remota creada

---
+++ src/sql/connection.js
@@ -0,0 +1,35 @@
+import mysql from "mysql2";
+
+const connection = mysql.createConnection({
+  host: "mysql-miapp1.alwaysdata.net",
+  user: "miapp1_prueba",
+  password: "R@@t2025",
+  database: "miapp1_crud"
+});
+
+
+connection.connect((error) => {
+  if (error) {
+    console.log("No se pudo conectar al servidor:", error.message);
+    return;
+  }
+  console.log("Conectado ");
+
+  
+  connection.query("SHOW TABLES", (err, results) => {
+    if (err) {
+      console.error("Error al mostrar tablas:", err.message);
+      return;
+    }
+
+    console.log("Tablas encontradas:");
+  
+
+    const tablas =results.map((fila)=> Object.values(fila)[0] )
+    console.log(tablas);
+
+  
+
+    connection.end();
+  });
+});
