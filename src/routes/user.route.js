3af0362a9a98596654f405f9f9369c68974f8e92
JNACHO2001 <113179500+JNACHO2001@users.noreply.github.com>
2025-08-17 12:16:59 -0500

    feat:creacion del endponit post de user

---
+++ src/routes/user.route.js
@@ -0,0 +1,48 @@
+import express from "express";
+
+import connection from "../sql/connection.js";
+
+
+const route = express();
+
+route.use(express.json())
+
+route.get("/", (req, resp) => {
+  const sql = "select * from users";
+
+  connection.query(sql, (err, resultado) => {
+    if (err) {
+      return resp
+        .status(500)
+        .json({ message: "no se pudo obtener los usuarios " });
+    }
+    resp.json(resultado);
+  });
+});
+
+route.post("/", (req, resp) => {
+  const sql = "insert into users (fullname,email,password,id_role) values (?,?,?,?)  ";
+  const {fullname,email,password,id_role} =req.body
+
+  connection.query(sql,[fullname,email,password,id_role], (err, resultado) => {
+    if (err) {
+      return resp
+        .status(500)
+        .json({ message: "no se pudo  insertar el usuario " });
+    }
+    resp.status(201).json({message:"usuario creado   ",Id:resultado.insertId  })
+  });
+});
+
+
+
+
+
+
+
+
+
+
+route.listen(3000, () => {
+  console.log(`Servidor corriendo en http://localhost:3000`);
+});
