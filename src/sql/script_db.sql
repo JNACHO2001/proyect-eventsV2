1037dde927d197ac0a40646556582242ab39d98d
JNACHO2001 <113179500+JNACHO2001@users.noreply.github.com>
2025-08-17 11:17:02 -0500

    feat:organizacion de la scritp

--- src/sql/script_db.sql
+++ src/sql/script_db.sql
@@ -1,41 +1,33 @@
-show databases;
-use miapp1_events;
-drop database  miapp1_events;
+drop database if exists miapp1_events;
 create database miapp1_events;
-
+use miapp1_events;
 
 create table roles(
-Id int auto_increment primary key,
-name varchar(100) unique 
+  Id int auto_increment primary key,
+  name varchar(100) unique not null
 );
 
 create table users (
-Id int auto_increment primary key ,
-fullname varchar(100),
-email varchar(100) unique,
-password varchar(100),
-confirm_password varchar(100),
-id_role int,
-foreign key (id_role) references roles(Id) 
-
+  Id int auto_increment primary key,
+  fullname varchar(100) not null,
+  email varchar(100) unique not null,
+  password varchar(100) not null,
+  id_role int not null,
+  foreign key (id_role) references roles(Id)
 );
+
 create table events(
-Id int auto_increment primary key,
-titulo varchar(100),
-descripcion text,
-fecha date,
-capacidad int
+  Id int auto_increment primary key,
+  titulo varchar(100) not null,
+  descripcion text,
+  fecha date not null,
+  capacidad int not null
 );
 
 create table participaciones (
-Id int auto_increment primary key,
-id_user int,
-id_event int,
-foreign key(id_user) references users(Id),
-foreign key(id_event) references events(Id)
-
-
+  Id int auto_increment primary key,
+  id_user int not null,
+  id_event int not null,
+  foreign key(id_user) references users(Id) on delete cascade on update cascade,
+  foreign key(id_event) references events(Id) on delete cascade on update cascade
 );
-
-show tables;
-
