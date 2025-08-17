ec6a61ce8ebd1a6ffd07c857ff66615b52c40cfd
JNACHO2001 <113179500+JNACHO2001@users.noreply.github.com>
2025-08-16 23:02:12 -0500

    feat:se modifico a pool

--- src/sql/connection.js
+++ src/sql/connection.js
@@ -1,30 +1,20 @@
 import mysql from "mysql2";
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
   host: "mysql-miapp1.alwaysdata.net",
   user: "miapp1_events",
   password: "R@@t2025",
   database: "miapp1_events",
 });
 
-connection.connect((error) => {
-  if (error) {
-    console.log("No se pudo conectar al servidor:", error.message);
+connection.query("SHOW TABLES", (err, results) => {
+  if (err) {
+    console.error("Error al mostrar tablas:", err.message);
     return;
   }
-  console.log("Conectado ");
 
-  connection.query("SHOW TABLES", (err, results) => {
-    if (err) {
-      console.error("Error al mostrar tablas:", err.message);
-      return;
-    }
+  console.log("Tablas encontradas:");
 
-    console.log("Tablas encontradas:");
-
-    const tablas = results.map((fila) => Object.values(fila)[0]);
-    console.log(tablas);
-
-    connection.end();
-  });
+  const tablas = results.map((fila) => Object.values(fila)[0]);
+  console.log(tablas);
 });
