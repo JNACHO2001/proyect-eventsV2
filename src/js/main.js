ea733e02914dd561985831f88ab3a7f1f53a4aaa
JNACHO2001 <113179500+JNACHO2001@users.noreply.github.com>
2025-08-18 18:39:06 -0500

    feat:creacion de la ruta principal y cargar en main

---
+++ src/js/main.js
@@ -0,0 +1,3 @@
+import { renderRoute } from "./routes";
+
+document.addEventListener('DOMContentLoaded',renderRoute)
--- src/js/routes.js
+++ src/js/routes.js
@@ -1,13 +1,27 @@
 const routes = {
   "/": {
-    path: "/src/view/login.html",
-    setup,
+    path: "/src/view/login.html"
+  
+  },
+  "/notfound":{
+    path:"src/view/notfound.html"
   }
 };
 
  export async function renderRoute() {
     const app = document.getElementById('app')
     const path = window.location.pathname
-    
+    const route = routes[path] || routes["/notfound"]
+
+    try {
+        const file = await fetch(route.path)
+        const content = await file.text()
+        app.innerHTML=content
+        
+    } catch (error) {
+        console.log("no encontre la ruta ",error)
+        
+    }
+
     
 }
