fd65e1cbe77cb2ef0555ccaddbb67a1d0ce3561b
JNACHO2001 <113179500+JNACHO2001@users.noreply.github.com>
2025-08-18 18:32:25 -0500

    feat:creacion del routes

---
+++ src/js/routes.js
@@ -0,0 +1,13 @@
+const routes = {
+  "/": {
+    path: "/src/view/login.html",
+    setup,
+  }
+};
+
+ export async function renderRoute() {
+    const app = document.getElementById('app')
+    const path = window.location.pathname
+    
+    
+}
