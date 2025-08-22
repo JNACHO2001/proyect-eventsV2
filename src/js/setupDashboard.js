bf5d647f12d782c783b13d2432a2e2e0e331cd4e
JNACHO2001 <113179500+JNACHO2001@users.noreply.github.com>
2025-08-22 12:52:24 -0500

    feat:renderizado de la informacion del usuario

--- src/js/setupDashboard.js
+++ src/js/setupDashboard.js
@@ -2,11 +2,22 @@ import { redirecto } from "./routes";
 
 export function setupDashboard() {
   setupOutButton();
+  infoUserSlide();
+}
+
+function infoUserSlide() {
+  const contentUserInfo = document.querySelector(".user-info");
+  const user = JSON.parse(localStorage.getItem("current"));
+  const firsLetter = user.fullname[0].toUpperCase();
+
+  contentUserInfo.innerHTML = ` <p class="user-avatar">${firsLetter}</p>
+        <h2 class="user-name">${user.fullname}</h2>
+        <p class="user-email">${user.email}</p>  `;
 }
 
 function setupOutButton() {
   const logout = document.querySelector(".logout-btn");
- logout.addEventListener("click", (e) => {
+  logout.addEventListener("click", (e) => {
     e.preventDefault();
     localStorage.removeItem("current");
     redirecto("/");
