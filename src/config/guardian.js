fd6713bd3e19fb80516b4c4f95aead01f51c3ba9
JNACHO2001 <113179500+JNACHO2001@users.noreply.github.com>
2025-08-24 18:37:27 -0500

    feat:arreglo de funciones innecesarias

--- src/config/guardian.js
+++ src/config/guardian.js
@@ -1,29 +1,10 @@
-
 export function getUser() {
   const user = localStorage.getItem("current");
   return user ? JSON.parse(user) : null;
 }
 
-
-export function isAuthenticated() {
-  return !!getUser();
-}
-
-export function isAdmin() {
-  const user = getUser();
-  return user && user.role === 1; 
-}
-
-
-export function isVisit() {
-  const user = getUser();
-  return user && user.role === 2; 
-}
-
-
 export function guard(path) {
   const user = getUser();
-
 
   if (!user) {
     return path === "/" || path === "/register";
@@ -32,13 +13,11 @@ export function guard(path) {
     return false;
   }
 
-
   if (user.role === 1 && path === "/dashboard") return true;
   if (user.role === 1 && path === "/dashboardVisit") return false;
 
   if (user.role === 2 && path === "/dashboardVisit") return true;
   if (user.role === 2 && path === "/dashboard") return false;
 
-  
   return false;
 }
